45c33d7e39a8c4d61b3e8addb056552fb66b0482
Angel Yam <amgelyam@gmail.com>
2021-11-24 22:52:32 -0600

    pokemon randon 87

--- lib/Pages/juego_page.dart
+++ lib/Pages/juego_page.dart
@@ -38,21 +38,51 @@ class _JuegoScreenState extends State<JuegoScreen> {
         });
   }
 
+  void pokemonRandom() {}
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
+    int count = Get.arguments['count'];
+
     var numRandom = new Random();
     int idRandom = numRandom.nextInt(151);
-    int count = Get.arguments['count'];
-    var img;
-    var name;
+    int idRandom2 = numRandom.nextInt(151);
+    int idRandom3 = numRandom.nextInt(151);
+    int idRandom4 = numRandom.nextInt(151);
+
+    var pokeRandom = [idRandom3, idRandom, idRandom2, idRandom3, idRandom4];
+    print(pokeRandom);
+
+    var img, name, name2, name3, name4;
+    var orden = [1, 2, 3, 4];
+
+    Random r = new Random();
+    for (int i = orden.length; i > 0; i--) {
+      int posicion = r.nextInt(i);
+      int tmp = orden[i - 1];
+      orden[i - 1] = orden[posicion];
+      orden[posicion] = tmp;
+    }
+    print(orden);
+
+    var imgRandom = new Random();
+    int ordenimg = imgRandom.nextInt(3);
+
     if (pokedex.length > 0) {
-      img = pokedex[idRandom]['img'];
-      name = pokedex[idRandom]['name'];
+      img = pokedex[pokeRandom[ordenimg]]['img'];
+      name = pokedex[pokeRandom[orden[0]]]['name'];
+      name2 = pokedex[pokeRandom[orden[1]]]['name'];
+      name3 = pokedex[pokeRandom[orden[2]]]['name'];
+      name4 = pokedex[pokeRandom[orden[3]]]['name'];
+      print(img);
     } else {
       img = "https://webstockreview.net/images/dot-clipart-bullet-point-9.png";
       name = "Pokemón";
+      name2 = "Pokemón";
+      name3 = "Pokemón";
+      name4 = "Pokemón";
     }
     //print(pokedex[75]['name']);
     return Scaffold(
@@ -119,9 +149,102 @@ class _JuegoScreenState extends State<JuegoScreen> {
                     //Get.toNamed("/pokedex");
                     //Get.toNamed("/juego");
                   },
-                  child: const Text(
-                    "Pokedex",
-                    style: TextStyle(
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.redAccent,
+                )),
+          ),
+          Positioned(
+            top: (height * 0.60),
+            left: (width / 2) - 170,
+            child: Container(
+                decoration: const BoxDecoration(
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.black54,
+                        blurRadius: 15.0,
+                        offset: Offset(0.0, 0.75))
+                  ],
+                ),
+                child: MaterialButton(
+                  height: 58,
+                  minWidth: 340,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  onPressed: () {
+                    //Get.toNamed("/pokedex");
+                    //Get.toNamed("/juego");
+                  },
+                  child: Text(
+                    name2,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.redAccent,
+                )),
+          ),
+          Positioned(
+            top: (height * 0.70),
+            left: (width / 2) - 170,
+            child: Container(
+                decoration: const BoxDecoration(
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.black54,
+                        blurRadius: 15.0,
+                        offset: Offset(0.0, 0.75))
+                  ],
+                ),
+                child: MaterialButton(
+                  height: 58,
+                  minWidth: 340,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  onPressed: () {
+                    //Get.toNamed("/pokedex");
+                    //Get.toNamed("/juego");
+                  },
+                  child: Text(
+                    name3,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.redAccent,
+                )),
+          ),
+          Positioned(
+            top: (height * 0.80),
+            left: (width / 2) - 170,
+            child: Container(
+                decoration: const BoxDecoration(
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.black54,
+                        blurRadius: 15.0,
+                        offset: Offset(0.0, 0.75))
+                  ],
+                ),
+                child: MaterialButton(
+                  height: 58,
+                  minWidth: 340,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  onPressed: () {
+                    //Get.toNamed("/pokedex");
+                    //Get.toNamed("/juego");
+                  },
+                  child: Text(
+                    name4,
+                    style: const TextStyle(
                       fontSize: 24,
                       color: Colors.white,
                     ),
